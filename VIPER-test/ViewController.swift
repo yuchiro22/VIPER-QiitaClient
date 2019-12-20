3a585801ed81d7f2b8bff1339110ab0b2bcbc940
yuichiro.tsuji <yuichiro.tsuji@excite.jp>
2019-12-20 17:40:00 +0900

    Initial Commit

---
+++ VIPER-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPER-test
+//
+//  Created by 辻 悠一郎 on 2019/12/20.
+//  Copyright © 2019 辻 悠一郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
